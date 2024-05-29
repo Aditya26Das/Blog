c9ed6519a78d612ace65a6ac93bdace03bfd01fb
Aditya26Das <aditya26.das@gmail.com>
2024-05-29 12:27:25 +0530

    Bugs Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import authServise from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 
 function App() {
-  return (
-    <>
-      <div>This is Appwrite</div>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authServise
+      .getCurrentUser()
+      .then((data) => {
+        if (data) {
+          dispatch(login({ data }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div>Not Loading</div>
+  ) : (<div>Loading</div>) ;
 }
 
-export default App
+export default App;
